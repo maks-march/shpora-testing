a12d43206d25ef813a5acf5a1b4c177ab60c2f94
marchenko <1maks_2055@mail.ru>
2025-10-29 02:20:04 +0500

    Refactor ДЗ Testing

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -8,69 +9,140 @@ public class ObjectComparison
     #region test data
     private static Person currentTsar = TsarRegistry.GetCurrentTsar();
     private static Person tsarCopy = new Person("Ivan IV The Terrible", 54, 170, 70, new Person("Vasili III of Russia", 28, 170, 60, null));
+    private static Person tsarCopyDifferent = new Person("Ivan IV The Kind", 54, 170, 70, new Person("Vasili III of Russia", 28, 170, 60, null));
+
     private static Person person0Parents = new Person("1", 1, 1, 1, null);
-    private static Person person0ParentsCopy = new Person("1", 1, 1, 1, null);
+    private static Person person0ParentsCopy = Person.Copy(person0Parents);
+    private static Person person0ParentsLimits = new Person(string.Empty, int.MaxValue, int.MinValue, 0, null);
+    private static Person person0ParentsLimitsCopy = Person.Copy(person0ParentsLimits);
     private static Person person1Parents = new Person("1", 1, 1, 1, new Person("2", 2, 2, 2, null));
-    private static Person person1ParentsСopy = new Person("1", 1, 1, 1, new Person("2", 2, 2, 2, null));
+    private static Person person1ParentsСopy = Person.Copy(person1Parents);
+    private static Person person1ParentsDifferent = new Person("1", 1, 1, 1, new Person("3", 3, 3, 3, null));
     private static Person person2Parents = new Person("1", 1, 1, 1, new Person("2", 2, 2, 2, new Person("3", 3, 3, 3, null)));
-    private static Person person2ParentsCopy = new Person("1", 1, 1, 1, new Person("2", 2, 2, 2, new Person("3", 3, 3, 3, null)));
+    private static Person person2ParentsCopy = Person.Copy(person2Parents);
+    private static Person person2ParentsDifferent = new Person("1", 1, 1, 1, new Person("2", 2, 2, 2, new Person("5", 5, 5, 5, null)));
     #endregion
     
-    private static IEnumerable<TestCaseData> TsarCheck_FailCases()
-    {
-        var tsarFailedCopy = new Person("Ivan IV The", 54, 170, 70, new Person("Vasili III of Russia", 28, 170, 60, null));
-
-        yield return new TestCaseData(currentTsar, tsarFailedCopy).SetName("Проверка неправильного царя");
-        yield return new TestCaseData(person0Parents, person1ParentsСopy).SetName("Разный родитель");
-        yield return new TestCaseData(person1Parents, person2ParentsCopy).SetName("Разный родитель у родителя");
-        yield return new TestCaseData(currentTsar, person0Parents).SetName("Полностью разные");
-    }
-
-    [Test, TestCaseSource(nameof(TsarCheck_FailCases))]
-    public void FailCheckCurrentTsar(Person? actualTsar, Person? expectedTsar)
-    {
-        Action act = () => AreEqualFuent(actualTsar, expectedTsar);
-        act.Should().Throw<AssertionException>();
-    }
-    
-    private static IEnumerable<TestCaseData> TsarCheck_Cases()
+    private static IEnumerable<TestCaseData> FieldCompare_SuccessCases()
     {
         yield return new TestCaseData(null, null).SetName("Пустые аргументы");
         yield return new TestCaseData(currentTsar, tsarCopy).SetName("Проверка царя");
         yield return new TestCaseData(person0Parents, person0ParentsCopy).SetName("Нет родителя");
         yield return new TestCaseData(person1Parents, person1ParentsСopy).SetName("Есть родитель");
-        yield return new TestCaseData(person2Parents, person2ParentsCopy).SetName("Родитель у родителя");
-    }  
-
-    [Test, TestCaseSource(nameof(TsarCheck_Cases))]
-    [Description("Проверка текущего царя")]
-    [Category("ToRefactor")]
-    public void CheckCurrentTsar(Person? actualTsar, Person? expectedTsar)
-    {
-        // Перепишите код на использование Fluent Assertions.
-        AreEqualFuent(actualTsar, expectedTsar);
+        yield return new TestCaseData(person0Parents, person1ParentsСopy).SetName("Разные родители");
+        yield return new TestCaseData(person2Parents, person2ParentsDifferent).SetName("Разные прародители");
+        yield return new TestCaseData(person0ParentsLimits, person0ParentsLimitsCopy).SetName("Проверка с граничными значениями полей");
     }
 
-    private void AreEqualFuent(Person? actual, Person? expected)
+    [Test, TestCaseSource(nameof(FieldCompare_SuccessCases))]
+    [Description("Проверка Person по полям - поля равны")]
+    public void AreEqual_NotThrows_OnEqualFields(Person? actual, Person? expected)
     {
         actual.Should().BeEquivalentTo(expected, options =>
             options
                 .Excluding(t => t.Id)
-                .Excluding(t => t.Parent) // Игнорировать Parent
+                .Excluding(t => t.Parent)
         );
-        if (actual != null && expected != null)
-        {
-            if (actual.Parent != null && expected.Parent != null)
-            {
-                AreEqualFuent(actual.Parent, expected.Parent);
-            }
-            if (actual.Parent == null || expected.Parent == null)
-            {
-                actual.Parent.Should().BeNull();
-                expected.Parent.Should().BeNull();
-            }
-        }        
+    }    
+    
+    private static IEnumerable<TestCaseData> FieldCompare_FailCases()
+    {
+        yield return new TestCaseData(currentTsar, null).SetName("Сравнение с null");
+        yield return new TestCaseData(currentTsar, tsarCopyDifferent).SetName("Проверка неправильного царя");
+        yield return new TestCaseData(currentTsar, person0Parents).SetName("Полностью разные");
+        yield return new TestCaseData(person0ParentsLimits, person0Parents).SetName("Просто разные");
     }
+
+    [Test, TestCaseSource(nameof(FieldCompare_FailCases))]
+    [Description("Проверка Person по полям - поля разные")]
+    public void AreEqual_Throws_OnDifferentFields(Person? actual, Person? expected)
+    {
+        actual.Should().NotBeEquivalentTo(expected, options =>
+            options
+                .Excluding(t => t.Id)
+                .Excluding(t => t.Parent)
+        );
+    }
+
+    private static IEnumerable<TestCaseData> FullCompare_SuccessCases()
+    {
+        yield return new TestCaseData(null, null).SetName("Пустые аргументы при полном сравнении");
+        yield return new TestCaseData(currentTsar, tsarCopy).SetName("Проверка царя при полном сравнении");
+        yield return new TestCaseData(person0Parents, person0ParentsCopy).SetName("Нет родителя при полном сравнении");
+        yield return new TestCaseData(person1Parents, person1ParentsСopy).SetName("Есть родитель при полном сравнении");
+        yield return new TestCaseData(person2Parents, person2ParentsCopy).SetName("Родитель у родителя при полном сравнении");
+    }
+
+
+    [Test, TestCaseSource(nameof(FullCompare_SuccessCases))]
+    [Description("Проверка Person по Parent - Parent равны")]
+    public void AreEqual_NotThrows_OnEqualPersons(Person? actual, Person? expected)
+    {
+        actual.Should().BeEquivalentTo(expected, options =>
+            options
+                .Excluding(t => t.Id)
+                .Using<Person>(ctx =>
+                    {
+                        AreEqual_NotThrows_OnEqualFields(ctx.Subject, ctx.Expectation);
+                        if (ctx.Subject != null && ctx.Expectation != null)
+                        {
+                            if (ctx.Subject.Parent != null && ctx.Expectation.Parent != null)
+                            {
+                                AreEqual_NotThrows_OnEqualPersons(ctx.Subject.Parent, ctx.Expectation.Parent);
+                            }
+                            else
+                            {
+                                ctx.Subject.Parent.Should().BeEquivalentTo(ctx.Expectation.Parent, options =>
+                                    options
+                                        .Excluding(t => t.Id)
+                                        .Excluding(t => t.Parent)
+                                );
+                            }
+                        }                        
+                    }
+                )
+                .WhenTypeIs<Person>()
+        ); 
+    }
+
+    private static IEnumerable<TestCaseData> FullCompare_FailCases()
+    {
+        yield return new TestCaseData(currentTsar, null).SetName("Сравнение с null при полном сравнении");
+        yield return new TestCaseData(currentTsar, tsarCopyDifferent).SetName("Проверка неправильного царя при полном сравнении");
+        yield return new TestCaseData(currentTsar, person1Parents).SetName("Полностью разные при полном сравнении");
+        yield return new TestCaseData(person1Parents, person0Parents).SetName("Родитель null при полном сравнении");
+        yield return new TestCaseData(person1Parents, person1ParentsDifferent).SetName("Разные родители при полном сравнении");
+        yield return new TestCaseData(person2Parents, person2ParentsDifferent).SetName("Разные прародители при полном сравнении");
+    }
+
+    [Test, TestCaseSource(nameof(FullCompare_FailCases))]
+    [Description("Проверка Person по Parent - Parent различны")]
+    public void AreEqual_Throw_OnDifferentPersons(Person? actual, Person? expected)
+    {
+        actual.Should().NotBeEquivalentTo(expected, options =>
+            options
+                .Excluding(t => t.Id)
+                .Using<Person>(ctx =>
+                    {
+                        AreEqual_NotThrows_OnEqualFields(ctx.Subject, ctx.Expectation);
+                        if (ctx.Subject != null && ctx.Expectation != null)
+                        {
+                            if (ctx.Subject.Parent != null && ctx.Expectation.Parent != null)
+                            {
+                                AreEqual_NotThrows_OnEqualPersons(ctx.Subject.Parent, ctx.Expectation.Parent);
+                            }
+                            else
+                            {
+                                (ctx.Subject.Parent == null && ctx.Expectation.Parent == null).Should().BeTrue();
+                            }
+                        }
+                    }
+                )
+                .WhenTypeIs<Person>()
+        );
+    }
+    
+    //Как будто бы если оставлять обобщенный метод кода становится меньше
 
     // При добавлении новых полей в Person придется расширять условие в return еще больше;
     // Тест должен пройти по всем Parent прежде чем дать результат, 
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -11,64 +11,87 @@ public class NumberValidatorTests
 {
     #region Constructor tests
 
-    private static IEnumerable<TestCaseData> Constructor_MustThrow()
+    private static IEnumerable<TestCaseData> Constructor_MustThrow_OnPrecisionExceed()
     {
-        yield return new TestCaseData(-1, 2, true);
-        yield return new TestCaseData(-1, 2, false);
-        yield return new TestCaseData(1, -2, true);
-        yield return new TestCaseData(-1, -2, false);
-        yield return new TestCaseData(1, 2, true);
-        yield return new TestCaseData(1, 1, false);
+        yield return new TestCaseData(0, 0, false).SetName("Некорректная длина");
+        yield return new TestCaseData(-1, -1, false).SetName("Некорректная длина");
+        yield return new TestCaseData(-2, 2, true).SetName("Некорректная длина");
+        yield return new TestCaseData(0, -3, false).SetName("Некорректная длина");
+        yield return new TestCaseData(-5, -5, false).SetName("Некорректная длина");
     }
 
-    private static IEnumerable<TestCaseData> Constructor_NotThrow()
+    private static IEnumerable<TestCaseData> Constructor_MustThrow_OnScaleExceed()
     {
-        yield return new TestCaseData(1, 0, true);
-        yield return new TestCaseData(int.MaxValue, int.MaxValue-1, true);
+        yield return new TestCaseData(1, 2, true).SetName("Некорректная точность");
+        yield return new TestCaseData(1, 1, false).SetName("Некорректная точность");
+        yield return new TestCaseData(1, -1, false).SetName("Некорректная точность");
     }
 
-    [Test, TestCaseSource(nameof(Constructor_MustThrow))]
-    public void Constructor_Throw_OnInvalidArgument(int precision, int scale, bool onlyPositive)
+    private static IEnumerable<TestCaseData> Constructor_NotThrow_OnValidArguments()
+    {
+        yield return new TestCaseData(1, 0, true).SetName("Корректные аргументы");
+        yield return new TestCaseData(int.MaxValue, int.MaxValue-1, true).SetName("Корректные аргументы");
+    }
+
+    [Test]
+    [
+        TestCaseSource(nameof(Constructor_MustThrow_OnPrecisionExceed)),
+        TestCaseSource(nameof(Constructor_MustThrow_OnScaleExceed))
+    ]
+    public void Constructor_Throws_OnInvalidArgument(int precision, int scale, bool onlyPositive)
     {
         Action action = () => new NumberValidator(precision, scale, onlyPositive);
         action.Should().Throw();
     }
 
-    [Test, TestCaseSource(nameof(Constructor_NotThrow))]
-    public void Constructor_NotThrow_OnValidArgument(int precision, int scale, bool onlyPositive)
+    [Test, TestCaseSource(nameof(Constructor_NotThrow_OnValidArguments))]
+    public void Constructor_NotThrows_OnValidArgument(int precision, int scale, bool onlyPositive)
     {
         Action action = () => new NumberValidator(precision, scale, onlyPositive);
         action.Should().NotThrow();
     }
-    
+
     #endregion
 
     #region IsValidNumber tests
 
-    private static IEnumerable<TestCaseData> CorrectNumbers_Cases()
+    private static IEnumerable<TestCaseData> IsValidNumbers_ValidNumbersCases()
     {
-        yield return new TestCaseData(17, 2, true, new string[] { "0.0", "0" });
+        yield return new TestCaseData(17, 2, true, new string[] { "0.0", "0", "012322121.23" }).SetName("Правильные числа");
 
-        yield return new TestCaseData(4, 2, false, new string[] { "-1.23", "12.32" });
-        
-        yield return new TestCaseData(4, 2, true, new string[] {"+1.23", "32"});
+        yield return new TestCaseData(4, 2, false, new string[] { "-1,23", "12,32" }).SetName("Правильные числа");
 
-        yield return new TestCaseData(20, 19, false, new string[] {"1.1234567890123456789", "1123456789012345678.9"});
-    }
-    private static IEnumerable<TestCaseData> IncorrectNumbers_Cases()
-    {
-        yield return new TestCaseData(3, 2, true, new string[] { "00.00", "-0.00", "a.sd", "+0.00", "+1.23" });
-        
-        yield return new TestCaseData(4, 2, true, new string[] {"-0.00", "-1.23"});
+        yield return new TestCaseData(4, 2, true, new string[] { "+1,23", "32" }).SetName("Правильные числа");
 
-        yield return new TestCaseData(17, 2, false, new string[] {"0.sd00", "O12322121.23"});
-
-        yield return new TestCaseData(19, 18, false, new string[] {"1.1234567890123456789", "12345678912345678900.01"});
-        
+        yield return new TestCaseData(20, 19, false, new string[] { "1,1234567890123456789", "1123456789012345678.9" }).SetName("Правильные числа");
     }
 
-    [Test, TestCaseSource(nameof(CorrectNumbers_Cases))]
-    public void IsValidNumber_CorrectNumber(int precision, int scale, bool onlyPositive, string[] numbers)
+    private static IEnumerable<TestCaseData> IsValidNumbers_TooBigNumbersCases()
+    {
+        yield return new TestCaseData(3, 2, false, new string[] { "00.00", "-0.00", "+0.00", "01,24" }).SetName("Слишком большие числа");
+
+        yield return new TestCaseData(19, 18, false, new string[] { "1.1234567890123456789", "12345678912345678900.01" }).SetName("Слишком большие числа");
+
+    }
+    private static IEnumerable<TestCaseData> IsValidNumbers_WithWrongSignCases()
+    {
+        yield return new TestCaseData(4, 2, true, new string[] { "-0.00", "-1.23" }).SetName("Числа с неправильным знаком");;
+        
+        yield return new TestCaseData(4, 3, true, new string[] { ".000", "+.124" }).SetName("Числа без целой части");;
+    }
+    
+    private static IEnumerable<TestCaseData> IsValidNumbers_WithLiteralsCases()
+    {
+        yield return new TestCaseData(4, 2, true, new string[] { "O.00", "_0.00"}).SetName("Некорректные символы");
+
+        yield return new TestCaseData(17, 2, false, new string[] {"0.sd00", "OIOIIOIO"}).SetName("Некорректные символы");;
+
+        yield return new TestCaseData(19, 18, false, new string[] {"1,12    0123456789", "1678912345678900 . 01", "#!@$:|><1,2?%^&*()"}).SetName("Некорректные символы");;
+        
+    }
+
+    [Test, TestCaseSource(nameof(IsValidNumbers_ValidNumbersCases))]
+    public void IsValidNumber_IsTrue_OnCorrectNumber(int precision, int scale, bool onlyPositive, string[] numbers)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
 
@@ -78,8 +101,13 @@ public class NumberValidatorTests
         }
     }
 
-    [Test, TestCaseSource(nameof(IncorrectNumbers_Cases))]
-    public void IsValidNumber_IncorrectNumber(int precision, int scale, bool onlyPositive, string[] numbers)
+    [Test]
+    [
+        TestCaseSource(nameof(IsValidNumbers_TooBigNumbersCases)),
+        TestCaseSource(nameof(IsValidNumbers_WithLiteralsCases)),
+        TestCaseSource(nameof(IsValidNumbers_WithWrongSignCases))
+    ]
+    public void IsValidNumber_IsFalse_OnIncorrectNumber(int precision, int scale, bool onlyPositive, string[] numbers)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
 
